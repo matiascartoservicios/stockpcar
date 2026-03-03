741958436f8431d5c547bd20ebd4d151aee80613
matiascartoservicios <matias.cartoservicios@gmail.com>
2026-03-02 23:01:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -58,10 +58,20 @@ try:
                         if pd.notna(row[col_name]) and str(row[col_name]).strip() != "":
                             fotos_extra.append(row[col_name])
                 
-                if fotos_extra:
+                    if fotos_extra:
                     with st.expander("📸 Ver más fotos"):
                         for url in fotos_extra:
                             st.image(url, use_container_width=True)
+                        
+                        # BOTÓN PARA VOLVER ARRIBA
+                        # Este link vacío "#" hace que el navegador suba
+                        st.markdown("""
+                            <a href="#stock-disponible" style="text-decoration:none;">
+                                <div style="background-color:#004080; color:white; padding:10px; text-align:center; border-radius:5px; font-weight:bold; margin-top:10px;">
+                                    ▲ VOLVER ARRIBA Y CERRAR
+                                </div>
+                            </a>
+                        """, unsafe_allow_html=True)
                 
               # 3. Título y Datos
                 km_texto = str(row['KM']).replace('.0', '')
@@ -84,6 +94,7 @@ try:
         st.info("No hay unidades que coincidan con esa búsqueda. ¡Probá con otra!")
 except Exception as e:
     st.error(f"Hubo un error al conectar con la base de datos: {e}")
+
 
 
 
